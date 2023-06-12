dc25f532974c5064b5edf77262112c94eabb3022
soulefSaoud <soulef.saoud@laposte.net>
2023-06-12 12:26:27 +0200

    readme en plus

--- src/main/java/com/m2i/tp/MyApp.java
+++ src/main/java/com/m2i/tp/MyApp.java
@@ -5,6 +5,7 @@ public class MyApp {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("lundi 12 juin 2023 12:14");
+		System.out.println("bientôt dejeuner");
 
 	}
 
