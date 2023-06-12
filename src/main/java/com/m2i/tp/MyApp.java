1c5c8ca65dd56710d00ca41f7abb347bfb960cb9
soulefSaoud <soulef.saoud@laposte.net>
2023-06-12 13:57:55 +0200

    autre commitA

--- src/main/java/com/m2i/tp/MyApp.java
+++ src/main/java/com/m2i/tp/MyApp.java
@@ -6,7 +6,7 @@ public class MyApp {
 		// TODO Auto-generated method stub
 		System.out.println("lundi 12 juin 2023 12:14");
 		System.out.println("bientôt dejeuner");
-		System.out.println("devB is the best");
+		System.out.println("devA is the best");
 
 	}
 
