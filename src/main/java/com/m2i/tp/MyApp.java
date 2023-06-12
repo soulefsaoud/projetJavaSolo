05abdf8b6af5bdb557d065f218067886c24cf300
soulefSaoud <soulef.saoud@laposte.net>
2023-06-12 11:58:49 +0200

    modif

--- src/main/java/com/m2i/tp/MyApp.java
+++ src/main/java/com/m2i/tp/MyApp.java
@@ -4,7 +4,7 @@ public class MyApp {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("lundi 12 juin 2023");
+		System.out.println("lundi 12 juin 2023 11h57");
 
 	}
 
