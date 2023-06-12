edc1d0c32a52463bd6979e6d85660dc7aa60eecd
soulefSaoud <soulef.saoud@laposte.net>
2023-06-12 11:34:51 +0200

    premier commit

---
+++ src/main/java/com/m2i/tp/MyApp.java
@@ -0,0 +1,11 @@
+package com.m2i.tp;
+
+public class MyApp {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("lundi 12 juin 2023");
+
+	}
+
+}
